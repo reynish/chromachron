fde05afaf631bcd0ff92bd03ac2101dbde6790bf
Alex Reynish <reynish@gmail.com>
2025-10-22 09:40:56 +0000

    Can we add coverage checking to the jest testing

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,10 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/lib/**/*.ts',
+    '!src/lib/placeholder-images.ts',
+    '!src/lib/utils.ts',
+  ],
 };
